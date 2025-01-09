061e6f55e775f216e15c38af0bff123a21943371
Bori Kokeny <bori@minerali.hu>
2025-01-09 12:59:58 +0100

    Adds random countries to fill if missing

---
+++ src/components/Countries.jsx
@@ -0,0 +1,11 @@
+export const countries = [
+  "Seychelles", 
+  "Romania", 
+  "Turkey", 
+  "Austria", 
+  "Chile", 
+  "Saint Maarten", 
+  "Montserrat", 
+  "Liberia", 
+  "Cook Islands",
+];
--- src/components/VenueList.jsx
+++ src/components/VenueList.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Media from "./media";
 import { LuMapPin } from "react-icons/lu";
+import { countries } from "./Countries";
+
+const getCountries = () => {
+  return countries[Math.floor(Math.random() * countries.length)];
+}
 
 export default function VenueList({ venues }) {
   if (!venues || venues.length === 0) {
@@ -9,7 +14,7 @@ export default function VenueList({ venues }) {
   }
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
+    <div className="max-w-5xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 p-4">
       {venues.map((venue) => {
         const { id, name, media, price, location } = venue;
 
@@ -36,9 +41,9 @@ export default function VenueList({ venues }) {
                 </div>
                 <div className="text-stone-800">
                   <h1 className="font-semibold mt-1">{name}</h1>
-                  <div className="flex">
-                  <LuMapPin className="mt-1 me-1"/>
-                  <h3>{location.country}</h3>
+                  <div className="flex items-center">
+                  <LuMapPin className="items-center  me-1"/>
+                  <h3>{location.country || getCountries()}</h3>
                   </div>
                   <h3><strong>{price} nok</strong>/night</h3>
                 </div>
