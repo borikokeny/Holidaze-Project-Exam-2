72ed7b0f6c40db392f7e1ff8ae026e60b29fa75b
Bori Kokeny <bori@minerali.hu>
2025-01-27 17:03:59 +0100

    Adds breakpoint to VenueCard

--- src/components/VenueCard.jsx
+++ src/components/VenueCard.jsx
@@ -77,9 +77,9 @@ const VenueCard = ({ venue, onDeleteSuccess }) => {
 
   return (
     <div className="font-main">
-      <div className=" bg-white p-4">
+      <div className="bg-white p-4">
         {Array.isArray(media) && media.length > 0 ? (
-          <Carousel className="w-full h-96">
+          <Carousel className="w-full h-64 sm:h-72 md:h-96">
             {media.map((image, index) => (
               <img
                 key={index}
@@ -92,110 +92,97 @@ const VenueCard = ({ venue, onDeleteSuccess }) => {
         ) : (
           <img
             src={placeholderImage}
-            className="w-full h-64 object-cover rounded-xl"
+            className="w-full h-48 sm:h-64 md:h-72 object-cover rounded-xl"
             alt={`${name} placeholder`}
           />
         )}
       </div>
-
-      <div className="flex ms-4">
-        <div className="w-1/2">
-          <h1 className="text-2xl font-bold text-stone-600">{name}</h1>
-          <div className="flex items-center">
-            <LuMapPin className="items-center  me-1" />
+      <div className="flex flex-col md:flex-row p-4 gap-8">
+        <div className="md:w-1/2">
+          <h1 className="text-xl sm:text-2xl font-bold text-stone-600">
+            {name}
+          </h1>
+          <div className="flex items-center mt-2">
+            <LuMapPin className="mr-2" />
             <h3>{randomLocation.country}</h3>
           </div>
-          <h2 className="w-4/6 text-justify text-lg mt-3 mb-3 text-stone-600">
+          <h2 className="text-base sm:text-lg mt-3 mb-3 text-stone-600">
             {description}
           </h2>
-          <hr className="border-t-2 border-gray-300 mb-1 w-4/6" />
-
-          <div className="flex items-center">
-            <div className="">
-              <img
-                src={owner.avatar.url}
-                alt="Avatar"
-                className="object-cover rounded-full mt-1 w-12 h-12"
-              />
-            </div>
-            <div className="ms-5 mt-2">
-              <p>Hosted by {owner.name}</p>
-              <p>{owner.email}</p>
+          <hr className="border-t-2 border-gray-300 mb-3" />
+          <div className="flex items-center gap-4">
+            <img
+              src={owner.avatar.url}
+              alt="Avatar"
+              className="object-cover rounded-full w-12 h-12"
+            />
+            <div>
+              <p className="text-sm sm:text-base">Hosted by {owner.name}</p>
+              <p className="text-sm sm:text-base">{owner.email}</p>
             </div>
           </div>
-
-          <hr className="border-t-2 border-gray-300 mt-3 mb-3 w-4/6" />
+          <hr className="border-t-2 border-gray-300 mt-3 mb-3" />
           <Ratings rating={venue.rating} />
-
-          <p className="text-lg mb-3 text-stone-600">
+          <p className="text-base sm:text-lg mt-3">
             Max guests: {maxGuests} person
           </p>
-          <p className="text-lg font-medium text-stone-600 mb-3">
+          <p className="text-base sm:text-lg font-medium mt-3">
             This place offers:
           </p>
-          <div className="table-row">
-            <div className="">
-              <p className="flex pe-2">
-                <FaWifi className="mt-1 me-3 mb-3 text-teal-500" />
-                {meta.wifi ? "Wifi" : "NO Wifi"}
-              </p>
-              <p className="flex pe-2">
-                <FaParking className="mt-1 me-3 text-teal-500" />
-                {meta.parking ? "Parking" : "NO Parking"}
-              </p>
-            </div>
-
-            <div className="table-cell">
-              <p className="flex pe-2">
-                <MdFreeBreakfast className="mt-1 me-3 mb-3 text-teal-500" />
-                {meta.breakfast ? "Breakfast" : "Breakfast is not included"}
-              </p>
-              <p className="flex pe-2">
-                <MdOutlinePets className="mt-1 me-3 text-teal-500" />
-                {meta.pets ? "Pets allowed" : "Sorry, pets are not allowed"}
-              </p>
-            </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 mt-3">
+            <p className="flex items-center">
+              <FaWifi className="mr-3 text-teal-500" />
+              {meta.wifi ? "Wifi" : "No Wifi"}
+            </p>
+            <p className="flex items-center">
+              <FaParking className="mr-3 text-teal-500" />
+              {meta.parking ? "Parking" : "No Parking"}
+            </p>
+            <p className="flex items-center">
+              <MdFreeBreakfast className="mr-3 text-teal-500" />
+              {meta.breakfast ? "Breakfast" : "Breakfast is not included"}
+            </p>
+            <p className="flex items-center">
+              <MdOutlinePets className="mr-3 text-teal-500" />
+              {meta.pets ? "Pets allowed" : "No pets allowed"}
+            </p>
           </div>
-
-          <p className="text-lg font-medium mt-3 text-stone-600">Address: </p>
-          <p className="">{randomLocation.address}</p>
+          <p className="text-base sm:text-lg font-medium mt-3">Address:</p>
+          <p className="text-stone-600">{randomLocation.address}</p>
           <p className="text-stone-600">{randomLocation.city}</p>
           <p className="text-stone-600">{randomLocation.zip}</p>
           <p className="text-stone-600">{randomLocation.country}</p>
         </div>
-        <div className="w-1/2">
-          <div className="flex justify-end me-4 mb-3">
+        <div className="md:w-1/2">
+          <div className="flex justify-end gap-2 mb-3">
             {userEmail === owner?.email && (
-              <div>
+              <>
                 <button
                   onClick={openForm}
-                  className="mt-4 px-4 py-2 bg-gray-700 text-white rounded hover:bg-gray-400"
+                  className="px-4 py-2 bg-gray-700 text-white rounded-none hover:bg-gray-400"
                 >
                   Edit Venue
                 </button>
                 <button
                   onClick={handleDelete}
-                  className="mt-4 ms-2 px-4 py-2 border-2 border-neutral-400 text-gray-600 rounded hover:bg-red-400 hover:text-gray-900"
+                  className="px-4 py-2 border-2 border-neutral-400 text-gray-600 rounded-none hover:bg-red-400 hover:text-gray-900"
                 >
                   Delete Venue
                 </button>
-              </div>
+              </>
             )}
           </div>
 
           <BookingForm venue={venue} />
         </div>
-
-        {openModal && (
-          <Modal onClose={closeForm}>
-            <VenueUpdateForm venue={venue} onClose={closeForm} />
-          </Modal>
-        )}
       </div>
+      {openModal && (
+        <Modal onClose={closeForm}>
+          <VenueUpdateForm venue={venue} onClose={closeForm} />
+        </Modal>
+      )}
     </div>
   );
 };
 
 export default VenueCard;
-
-
