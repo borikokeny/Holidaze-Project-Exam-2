8467902285594ea0b921e8b2b713e153ed447ac4
Bori Kokeny <bori@minerali.hu>
2025-01-27 16:41:14 +0100

    Adjusts modal width

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -9,19 +9,13 @@ const Modal = ({ children, onClose }) => {
   
   return ReactDOM.createPortal(
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="relative bg-white p-6 rounded shadow-lg w-2/3">
+      <div className="relative bg-white p-6 rounded shadow-lg w-1/6">
         <button
           onClick={onClose}
           className="btn-primary absolute top-2 right-2 text-gray-500 hover:text-gray-800"
         >
           X
         </button>
-        {/* <button
-          onClick={onClose}
-          className="btn-secondary absolute top-2 right-2 text-gray-500 hover:text-gray-800"
-        >
-          X
-        </button> */}
         {children}
       </div>
     </div>,
